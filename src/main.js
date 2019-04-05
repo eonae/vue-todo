6253c4a6753778a2cbeb2c2f9bf12455d7bea0c8
eonae <eonae.white@gmail.com>
2019-04-05 22:42:16 +0300

    in process

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueBar from 'vuebar'
+import VueRouter from 'vue-router'
 
 import 'materialize-css'
 import 'materialize-css/dist/css/materialize.css'
 
+Vue.use(VueRouter);
 Vue.use(VueBar);
 
 Vue.config.productionTip = false;
